2a76d172170acffcd16dcc6d75c6bc914ab6d572
Anton <ant.band@mail.ru>
2025-09-13 17:38:38 +0300

    ^%@&^%#

--- infra/kube-config.tf
+++ infra/kube-config.tf
@@ -1,6 +1,6 @@
 # 
 resource "null_resource" "kube_config" {
-  depends_on = [ yandex_kubernetes_cluster.cluster ]
+  depends_on = [ yandex_kubernetes_cluster.cluster, yandex_kubernetes_node_group.ng-1 ]
   provisioner "local-exec" {
     command = "yc managed-kubernetes cluster get-credentials --id ${yandex_kubernetes_cluster.cluster.id} --external --force"
   }
